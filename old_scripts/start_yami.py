9bbc71133434dd7c7fe811f181b9258cca7a852f
Diego de Melo <diego-gonzaga@live.com>
2021-04-24 03:21:37 -0300

    Archiving scripts and creating a docker container to Don't Starve Together.

---
+++ old_scripts/start_yami.py
@@ -0,0 +1,13 @@
+import os
+
+cluster = "yami"
+os.chdir(
+    "/home/diego-gonzaga/.local/share/Steam/steamapps/common/Don't Starve Together Dedicated Server/bin/"
+)
+command = (
+    f"./dontstarve_dedicated_server_nullrenderer -console -cluster {cluster} -shard"
+)
+shard = "Master"
+os.system(f"gnome-terminal -- {command} {shard}")
+shard = "Caves"
+os.system(f"gnome-terminal -- {command} {shard}")
